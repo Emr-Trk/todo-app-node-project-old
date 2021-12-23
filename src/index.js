1ed1b6fa22da4de02f7eace3b8991642ff64ad13
hkn-asln <67700643+hkn-asln@users.noreply.github.com>
2021-12-23 14:55:21 +0000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -21,6 +21,24 @@ db.init().then(() => {
     process.exit(1);
 });
 
+db.init().then(() => {
+    app.listen(3000, () => console.log('Listening on port 3000'));
+}).catch((err) => {
+    console.error(err);
+    process.exit(1);
+});
+db.init().then(() => {
+    app.listen(3000, () => console.log('Listening on port 3000'));
+}).catch((err) => {
+    console.error(err);
+    process.exit(1);
+});
+db.init().then(() => {
+    app.listen(3000, () => console.log('Listening on port 3000'));
+}).catch((err) => {
+    console.error(err);
+    process.exit(1);
+});
 const gracefulShutdown = () => {
     db.teardown()
         .catch(() => {})
